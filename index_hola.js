ec91d7c018fb25c272410bb2cfce622a50b99afb
Claudia <estudiantecolegio373@gmail.com>
2026-09-13 00:07:54 -0400

    Aclaraciones, y declaración de variables

---
+++ index_hola.js
@@ -0,0 +1,14 @@
+//existen diversas formas en las que se declaran 
+//variables
+
+//let: 
+
+let index = ("Hola mundo");
+
+console.log (index); 
+
+//const, o constante, una valor que nunca se cambia
+
+let hacer = ("1 vez"); 
+
+//y más valores que ya mostraré  
