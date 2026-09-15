f93bb4995d07d133fce20ace9ddb77da1f7a50d0
Claudia <estudiantecolegio373@gmail.com>
2026-09-15 05:50:52 -0400

    Mi pequeña práctica con las variables

---
+++ index_variables.js
@@ -0,0 +1,19 @@
+/*Hola a todos para comentar un poquito, estaré publicando
+algunas partes, de los ejercios que estaré practicando, desenme buen 
+inicio. */ 
+
+let numero = (1, 2, 3, 4, 5);
+console.log(numero);
+
+/*Bueno para explicar un poco, la palabra "let" ayuda a crear un variable, es decir:
+declararla, anunciar de que existe, y una variable, es un espacio en donde puedes
+almacenar valores de diferentes tipos, ahí entra lo que sería los tipos de datos */
+
+let numero2 = (0, 10, 14); //Estos son del tipo number, numeros 
+console.log(numero2);
+let numero3 = (2.4, 16.4); //También los floats, los decimales (espero tener une este concepto correcto)
+console.log(numero3);
+//Luego los strings, booleanos, con el verdadero y falso, y los strings que permite todo en comillas
+
+//Para más tarde les muestro los tipos de datos, por ahora estemos con variables
+
