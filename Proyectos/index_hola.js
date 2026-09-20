2ccc20817d31fbd89b9ca574bd00f46cbb7d53da
Claudia <estudiantecolegio373@gmail.com>
2026-09-19 23:39:27 -0400

    Prácticar ordenadas, y organizadas

---
+++ Proyectos/index_hola.js
@@ -0,0 +1,12 @@
+//existen diversas formas en las que se declaran 
+//variables
+
+//let: 
+
+let index = ("Hola mundo");
+
+console.log (index); 
+
+//const, o constante, una valor que nunca se cambia
+const my_hello_true = ("Hola verdadero!")
+//y más valores que ya mostraré  
