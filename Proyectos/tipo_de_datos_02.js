75d633aacab0e49148de7bc494c71e754c4953ce
Claudia <estudiantecolegio373@gmail.com>
2026-09-22 02:58:58 -0400

    Una aclaración con los strings, o las cadenas de texto

---
+++ Proyectos/tipo_de_datos_02.js
@@ -0,0 +1,15 @@
+// Inciando con la clase (perdí la cuenta) tipo de datos 2
+
+//Como mencioné anteriormente, existen distintas formas de crear un String
+
+let nombre = "Soledad";
+console.log(nombre);
+
+nombre = 'Barboza'
+console.log(nombre); //de ambas formas
+
+//y la cadenas son inmutables, es decir, que cuando se crean no se pueden cambiar, cuando ocurre
+//esto muestra error 
+
+/*Si quiero crear otra cadena con gusto puedo crear otra y asignar los datos */
+//clase 2 de tipo de datos, end. 
